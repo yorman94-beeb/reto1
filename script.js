662fbfc4a8222356d390fc39314f3b35a93d9030
Yorman <cardonagallego@gmail.com>
2024-08-16 12:18:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let numeroUn = 1;
+let stringUn = '1';
+
+let numeroTreinta = 30;
+let stringTreinta = '30';
+
+let numeroDiez = 10;
+let stringDiez = '10';
+
+
+
+if (numeroUn == stringUn)//== solo compara el valor y no el tipo
+     {
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+  
+  } else {
+  
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+
+  
+  }
+
+
+
+
+
+  if (numeroTreinta === stringTreinta) // === compara el valor y la tipologia
+    {
+
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+  
+  } else {
+  
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+  
+  }
+
+
+
+
+
+
+  if ( numeroDiez ==stringDiez)//== compara solo el valor
+     {
+
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+  
+  } else {
+  
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+  
+  }
